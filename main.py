4fa4d5a11914b68dd09700a7e8dec46e38f343d3
Anthony Terrano <61809326+Terranova-Python@users.noreply.github.com>
2023-07-19 12:30:20 -0400

    Update main.py
    Added more comments for clarity

--- main.py
+++ main.py
@@ -10,24 +10,31 @@ import math
 customtkinter.set_appearance_mode("System")  # Modes: system (default), light, dark
 customtkinter.set_default_color_theme("blue")  # Themes: blue (default), dark-blue, green
 
+##### Tkinter stuff ######
 root = customtkinter.CTk()
 root.title('Music Player')
 root.geometry('400x480')
 pygame.mixer.init()
+##########################
 
-list_of_songs = ['music/The Search.wav', 'music/City.wav', 'music/Coffee Shop.wav']
-list_of_covers = ['img/nf.jpg', 'img/city.jpg', 'img/coffee.jpg']
+list_of_songs = ['music/City.wav'] # Add more songs into this list, make sure they are .wav and put into the music Directory.
+list_of_covers = ['img/city.jpg'] # Add more JPEGS into the img directory, You can download city from my Github as a starting point.
 n = 0
 
 def get_album_cover(song_name, n):
     image1 = Image.open(list_of_covers[n])
     image2=image1.resize((250, 250))
     load = ImageTk.PhotoImage(image2)
+    
     label1 = tkinter.Label(root, image=load)
     label1.image = load
     label1.place(relx=.19, rely=.06)
 
-    stripped_string = song_name[6:-4]
+    stripped_string = song_name[6:-4] #This is to exlude the other characters
+                                                # 6       :      -4
+                                    # Example: 'music/ | City | .wav'
+                                    # This works because the music will always be between those 2 values
+    
     song_name_label = tkinter.Label(text = stripped_string, bg='#222222', fg='white')
     song_name_label.place(relx=.4, rely=.6)
 
@@ -59,6 +66,7 @@ def play_music():
     n += 1
 
 def skip_forward():
+    # As an idea, you can turn play_music() into a start/pause function and create a seperate skip ahead function for this!
     play_music()
 
 def skip_back():
@@ -67,11 +75,11 @@ def skip_back():
     play_music()
 
 def volume(value):
-    #print(value)
+    #print(value) # If you care to see the volume value in the terminal, un-comment this :)
     pygame.mixer.music.set_volume(value)
 
 
-# Buttons
+# All Buttons
 play_button = customtkinter.CTkButton(master=root, text='Play', command=play_music)
 play_button.place(relx=0.5, rely=0.7, anchor=tkinter.CENTER)
 
